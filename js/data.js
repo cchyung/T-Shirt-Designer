b2ed541e06fd394df9db70ff0b0f159151712164
Conner Chyung <chyungspice@gmail.com>
2018-08-25 18:12:09 -0700

    restructure files

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const ROOT_URL = 'https://ecocentric-tshirt-designer.herokuapp.com/api/'
+
+function fetchStyles(callback) {
+  jQuery.get(ROOT_URL + 'styles', function(data) {
+    callback(data);
+  })
+}
+
+function fetchStyleImage(styleID, color_slug, callback) {
+  jQuery.get(
+    ROOT_URL + 'styles/' + styleID + '/images/' + color_slug,
+    function(data) {
+      callback(data)
+    }
+  );
+}
+
+function fetchAddons(callback){
+  jQuery.get(
+    ROOT_URL + 'addons/', function(data){
+      callback(data)
+    }
+  )
+}
+
+// function fetchImages(styleID, color, callback){
+//   callback({
+//     front: frontImageUrl,
+//     back: backImageUrl
+//   })
+// }
